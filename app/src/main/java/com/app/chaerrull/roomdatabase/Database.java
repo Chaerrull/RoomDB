5ce63980f2d329a1c2bf49d2f59f5a7b377d46dc
Chaerrull <62976741+Chaerrull@users.noreply.github.com>
2020-05-01 07:23:53 +0700

    Update Database.java

--- app/src/main/java/com/app/chaerrull/roomdatabase/Database.java
+++ app/src/main/java/com/app/chaerrull/roomdatabase/Database.java
@@ -1,5 +1,11 @@
 package com.app.chaerrull.roomdatabase;
 import androidx.room.RoomDatabase;
+/*
+Mohammad Chaerrull Febriansyah
+10117171 - IF5
+
+01 Mei 2020 : Membuat Keseluruhan Aplikasi dan Memperbaiki Error
+ */
 
 @androidx.room.Database(entities = {Entitas.class}, version = 2, exportSchema = false)
         public abstract class Database extends RoomDatabase {
